673e743f316121062f5c2920203f3300a31fa8ef
Aashish Adhikari <aashishadhikari@Aashishs-Mac-mini.local>
2026-01-30 02:56:34 +0545

    fixing bugs

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,9 +1,8 @@
 // lib/utils.ts
-import { type ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+// Simple className merger (no external deps needed)
+export function cn(...classes: (string | undefined | null | false)[]) {
+  return classes.filter(Boolean).join(' ');
 }
 
 // Date utilities
